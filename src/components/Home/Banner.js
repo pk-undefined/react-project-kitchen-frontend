a7313c1c2ad0f84bd0cbea85862829980343715a
ysemenyuk <invent@list.ru>
2021-08-28 00:13:54 +0300

    feat: styled banner

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Banner = ({ appName, token }) => {
-  if (token) {
-    return null;
-  }
+const Title = styled.h1`
+  color: #ff0000;
+  text-shadow: 2px 2px 0px #ffffff;
+  margin-bottom: 8px;
+`;
+
+const StyledBanner = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 32px 16px;
+  background: #0a0a0a;
+  box-shadow: inset 0px -1px 0px #1f1f1f;
+  font-size: 16px;
+  line-height: 28px;
+  font-family: 'Press Start 2P';
+  text-align: center;
+`;
+
+const Banner = (props) => {
   return (
-    <div className="banner">
-      <div className="container">
-        <h1 className="logo-font">
-          {appName.toLowerCase()}
-        </h1>
-        <p>Your community project starter pack.</p>
-      </div>
-    </div>
+    <StyledBanner {...props}>
+      <Title>Проектная кухня</Title>
+      Место, где готовится новый опыт
+    </StyledBanner>
   );
 };
 
